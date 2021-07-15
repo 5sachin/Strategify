328be45938f2c2708da65fa60f5a5f818c132fed
5sachin <sachinmishra10072000@gmail.com>
2021-07-15 21:49:32 +0530

    Second Commit

--- script.js
+++ script.js
@@ -1,17 +1,2 @@
-
-  $(document).ready(function(){
-  $(window).scroll(function(){
-  if($(window).scrollTop() > 60 ){
-  $('.my-navbar').addClass('navbar-scroll');
-  $('.nav-item a').css('color', 'black');
-  $('.navbar-brand').css('color', 'black');
-  }
-  else{
-  $('.my-navbar').removeClass('navbar-scroll');
-  $('.navbar-brand').css('color','white');
-  $('.nav-item a').css('color', 'white');
-  }
-  });
-  });
   
 
