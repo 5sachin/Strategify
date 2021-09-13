af329c62994bb84e69e035d5c2c25494e5713075
Sachin Mishra <sachinmishra10072000@gmail.com>
2021-09-13 17:27:24 +0530

    Update

--- script.js
+++ script.js
@@ -47,9 +47,9 @@ $(document).ready(function() {
 });
 
 function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("mySidenav").style.display = "inline-block";
 }
 
 function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("mySidenav").style.display = "none";
 }
