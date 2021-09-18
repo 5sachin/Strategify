1d880cc51ca8dfadfb03d8fe4c7b7f508b7b41c8
Sachin Mishra <sachinmishra10072000@gmail.com>
2021-09-18 17:43:01 +0530

    Ui update

--- script.js
+++ script.js
@@ -12,32 +12,38 @@ $(document).ready(function() {
         e.preventDefault();
         if (x < max_fields) {
             x++;
-            $(wrapper1).append('<div class="col-lg-4 col-xl-4 col-md-12 col-sm-12 col-xs-12"><input type="text" name="Strategyname" placeholder="Indicator" class="form-control"></div><div class="col-lg-3 col-xl-3 col-md-12 col-sm-12 col-xs-12"><input type="text" name="Strategyname" placeholder="Comparator" class="form-control"></div><div class="col-lg-4 col-xl-4 col-md-12 col-sm-12 col-xs-12"><input type="text" name="Strategyname" placeholder="Indicator" class="form-control"></div><div class="col-1"><span><a href="#" class="w-50 h-50 deleteEntryRow" style="font-size:3rem">&times;</a></span></div>'); //add input box
+            $(wrapper1).append('<div class="row" id="inputEntryFormRow"><div class="col-lg-4 col-xl-4 col-md-12 col-sm-12 col-xs-12"><input type="text" name="Strategyname" placeholder="Indicator" class="form-control"></div><div class="col-lg-3 col-xl-3 col-md-12 col-sm-12 col-xs-12"><input type="text" name="Strategyname" placeholder="Comparator" class="form-control"></div><div class="col-lg-4 col-xl-4 col-md-12 col-sm-12 col-xs-12"><input type="text" name="Strategyname" placeholder="Indicator" class="form-control"></div><div class="col-1"><span><a href="#" class="w-50 h-50 deleteEntryRow" style="font-size:3rem">&times;</a></span></div></div>'); //add input box
         } else {
             alert('You Reached the limits')
         }
     });
 
-    $(wrapper1).on("click", ".deleteEntryRow", function(e) {
-        e.preventDefault();
-        $(this).parent('div').remove();
+
+
+    $(document).on("click", ".deleteEntryRow", function() {
+        $(this).closest('#inputEntryFormRow').remove();
         x--;
-    })
+    });
+
+
+
+
+
+
 
     var x = 1;
     $(add_button2).click(function(e) {
         e.preventDefault();
         if (x < max_fields) {
             x++;
-            $(wrapper2).append('<div class="col-lg-4 col-xl-4 col-md-12 col-sm-12 col-xs-12"><input type="text" name="Strategyname" placeholder="Indicator" class="form-control"></div><div class="col-lg-3 col-xl-3 col-md-12 col-sm-12 col-xs-12"><input type="text" name="Strategyname" placeholder="Comparator" class="form-control"></div><div class="col-lg-4 col-xl-4 col-md-12 col-sm-12 col-xs-12"><input type="text" name="Strategyname" placeholder="Indicator" class="form-control"></div><div class="col-1"><span><a href="#" class="w-50 h-50" style="font-size:3rem">&times;</a></span></div>'); //add input box
+            $(wrapper2).append('<div class="row" id="inputExitFormRow"><div class="col-lg-4 col-xl-4 col-md-12 col-sm-12 col-xs-12"><input type="text" name="Strategyname" placeholder="Indicator" class="form-control"></div><div class="col-lg-3 col-xl-3 col-md-12 col-sm-12 col-xs-12"><input type="text" name="Strategyname" placeholder="Comparator" class="form-control"></div><div class="col-lg-4 col-xl-4 col-md-12 col-sm-12 col-xs-12"><input type="text" name="Strategyname" placeholder="Indicator" class="form-control"></div><div class="col-1"><span><a href="#" class="w-50 h-50 deleteExitRow" style="font-size:3rem">&times;</a></span></div></div>'); //add input box
         } else {
             alert('You Reached the limits')
         }
     });
 
-    $(wrapper2).on("click", ".delete", function(e) {
-        e.preventDefault();
-        $(this).parent('div').remove();
+    $(document).on("click", ".deleteExitRow", function(e) {
+        $(this).closest('#inputExitFormRow').remove();
         x--;
     })
 
@@ -53,3 +59,14 @@ function openNav() {
 function closeNav() {
   document.getElementById("mySidenav").style.display = "none";
 }
+
+$('#positionDropdown a').on('click', function(){
+    $('#positionDropBtn').text($(this).html());
+    /*$('#positionDropBtn').val($(this).html());*/
+});
+
+$('#timeframeDropDown a').on('click', function(){
+    $('#scripTimeFrameDropBtn').text($(this).html());
+    /*$('#positionDropBtn').val($(this).html());*/
+});
+
