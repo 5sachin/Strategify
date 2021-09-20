141d503047c2359ad17e5324f61dbd6fc769f3b8
Sachin Mishra <sachinmishra10072000@gmail.com>
2021-09-20 13:28:20 +0530

    UI Update

--- script.js
+++ script.js
@@ -61,64 +61,51 @@ function closeNav() {
 }
 
 
-var arr = ["john", "bob", "kayla", "sam", "elena", "josh", "andrew", "maria"];
-var selectValues = {
-  "1": "SBIN",
-  "2": "Reliance",
-  "3": "Titan",
-  "4": "TCS",
-  "5": "Adani",
-  "6": "Dr Reddy",
-  "7": "Titan",
-  "8": "TCS",
-  "9": "Adani",
-  "10": "Dr Reddy"
-};
 
 
-let suggestion = ["Relicane","SBIN","TCS","LTI","Tata Motors","MRF"];
+let suggestions = ["Relicane","SBIN","TCS","LTI","Tata Motors","Aditya Birla Capital Ltd","Ajanta Pharma Ltd","ACC Ltd","Adani Total Gas Ltd","Adani Green Energy Ltd","Adani Transmission Ltd","MRF","HDFC","Yes Bank","Adani","Titan","Bharti Airtel","Tata Steel","Infosys"];
 const searchWrapper = document.querySelector(".search-input");
 const inputBox = searchWrapper.querySelector("input");
-const sugBox = searchWrapper.querySelector(".autocom-box");
+const suggBox = searchWrapper.querySelector(".autocom-box");
+const icon = searchWrapper.querySelector(".icon");
+let linkTag = searchWrapper.querySelector("a");
+let webLink;
 
 
 inputBox.onkeyup = (e)=>{
-    console.log(e.target.value);
     let userData = e.target.value;
-    let emptyarray = [];
+    let emptyArray = [];
     if(userData){
-        emptyarray = suggestion.filter((data)=>{
+        emptyArray = suggestions.filter((data)=>{//filtering array value and user characters to lowercase and return only those words which are start with user enetered chars
             return data.toLocaleLowerCase().startsWith(userData.toLocaleLowerCase());
-
         });
-
-        emptyarray = emptyarray.map((data)=>{
-            return data='<li>'+ data+'</li>';
+        emptyArray = emptyArray.map((data)=>{
+            // passing return data inside li tag
+            return data = `<li>${data}</li>`;
         });
         searchWrapper.classList.add("active");
-        showSuggestion(emptyarray);
-        let allList = sugBox.querySelector("li");
-        for(let index =0;index<allList.length;i++){
-            allList[i].setAttribute("onclick","select(this)");
+        showSuggestions(emptyArray);
+        let allList = suggBox.querySelectorAll("li");
+        for (let i = 0; i < allList.length; i++) {
+            //adding onclick attribute in all li tag
+            allList[i].setAttribute("onclick", "select(this)");
         }
     }else{
-        searchWrapper.classList.remove("active");
+        searchWrapper.classList.remove("active"); //hide autocomplete box
     }
-    
 }
-
 function select(element){
-    let selectUserData = element.textContent;
-    console.log(selectUserData);
+    let selectData = element.textContent;
+    inputBox.value = selectData
+    searchWrapper.classList.remove("active");
 }
-
-function showSuggestion(list){
+function showSuggestions(list){
     let listData;
     if(!list.length){
         userValue = inputBox.value;
-        listData = '<li>'+ userValue +'</li>';
+        listData = `<li>${userValue}</li>`;
     }else{
-        listData = list.join('');
+      listData = list.join('');
     }
-    sugBox.innerHTML = listData;
+    suggBox.innerHTML = listData;
 }
