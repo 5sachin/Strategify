e687cd97ba4226b28c624f9de3cb5d87d910faad
5sachin <sachinmishra10072000@gmail.com>
2023-03-20 12:00:33 +0530

    Changes Autocomplete

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+/*
 $(document).ready(function() {
     var max_fields = 10;
     var wrapper1 = $(".entryConditionRow");
@@ -7,10 +7,11 @@ $(document).ready(function() {
     var add_button2 = $(".addExitRow");
 
 
-    var x = 1;
+    var x = 2;
     $(add_button1).click(function(e) {
         e.preventDefault();
-        $(wrapper1).append('<div class="row" id="inputEntryFormRow"><div class="col-lg-4 col-xl-4 col-md-12 col-sm-12 col-xs-12"><input type="text" name="Strategyname" placeholder="Indicator" class="form-control"></div><div class="col-lg-3 col-xl-3 col-md-12 col-sm-12 col-xs-12"><input type="text" name="Strategyname" placeholder="Comparator" class="form-control"></div><div class="col-lg-4 col-xl-4 col-md-12 col-sm-12 col-xs-12"><input type="text" name="Strategyname" placeholder="Indicator" class="form-control"></div><div class="col-1"><span class="w-50 h-50 deleteEntryRow" style="font-size:3rem">&times;</span></div></div>');
+        $(wrapper1).append('<div class="row" id="inputEntryFormRow"><div class="col-lg-4 col-xl-4 col-md-12 col-sm-12 col-xs-12"><div class="indicator-search"><input type="text" name="entryfirindicator'+x+'" placeholder="Indicator" class="form-control" autocomplete="off" id="entryfirindicator'+x+'" onkeyup="lookup(this);"><ul class="indicatorsuggest-box" id="firindicatorBox'+x+'"></ul></div></div><div class="col-lg-3 col-xl-3 col-md-12 col-sm-12 col-xs-12"><input type="text" name="Strategyname" placeholder="Comparator" class="form-control"></div><div class="col-lg-4 col-xl-4 col-md-12 col-sm-12 col-xs-12"><input type="text" name="entrysecindicator'+x+'" id="entrysecindicator'+x+'" placeholder="Indicator" class="form-control"></div><div class="col-1"><span class="w-50 h-50 deleteEntryRow" style="font-size:3rem">&times;</span></div></div>');
+        x++;
     });
 
 
@@ -20,10 +21,14 @@ $(document).ready(function() {
 
 
 
-    var x = 1;
+    var x = 2;
     $(add_button2).click(function(e) {
         e.preventDefault();
-            $(wrapper2).append('<div class="row" id="inputExitFormRow"><div class="col-lg-4 col-xl-4 col-md-12 col-sm-12 col-xs-12"><input type="text" name="Strategyname" placeholder="Indicator" class="form-control"></div><div class="col-lg-3 col-xl-3 col-md-12 col-sm-12 col-xs-12"><input type="text" name="Strategyname" placeholder="Comparator" class="form-control"></div><div class="col-lg-4 col-xl-4 col-md-12 col-sm-12 col-xs-12"><input type="text" name="Strategyname" placeholder="Indicator" class="form-control"></div><div class="col-1"><span class="w-50 h-50 deleteExitRow" style="font-size:3rem">&times;</span></div></div>'); //add input box
+            $(wrapper2).append('<div class="row" id="inputExitFormRow"><div class="col-lg-4 col-xl-4 col-md-12 col-sm-12 col-xs-12">'+
+                               '<input type="text" name="exitindiactorone" placeholder="Indicator" class="form-control">'+
+                               '<ul class="indicatorsuggest-box" id="indicatorBox"></ul>'+
+                               '</div><div class="col-lg-3 col-xl-3 col-md-12 col-sm-12 col-xs-12">'+
+                               '<input type="text" name="exitcomparator" placeholder="Comparator" class="form-control"></div><div class="col-lg-4 col-xl-4 col-md-12 col-sm-12 col-xs-12"><input type="text" name="exitindiactortwo" placeholder="Indicator" class="form-control"></div><div class="col-1"><span class="w-50 h-50 deleteExitRow" style="font-size:3rem">&times;</span></div></div>'); //add input box
     });
 
     $(document).on("click", ".deleteExitRow", function(e) {
@@ -34,7 +39,7 @@ $(document).ready(function() {
 
 
 });
-
+*/
 function openNav() {
   document.getElementById("mySidenav").style.display = "inline-block";
 }
@@ -53,10 +58,9 @@ const inputBox = searchWrapper.querySelector("input");
 const suggBox = searchWrapper.querySelector(".autocom-box");
 const icon = searchWrapper.querySelector(".icon");
 let linkTag = searchWrapper.querySelector("a");
-let webLink;
-
 
 inputBox.onkeyup = (e)=>{
+
     let userData = e.target.value;
     let emptyArray = [];
     if(userData){
@@ -80,6 +84,7 @@ inputBox.onkeyup = (e)=>{
 }
 
 var scripAddedContainer = document.querySelector(".addedscripList");
+
 function addScripName(element){
 
     var scripAddedName = document.createElement("a");
@@ -121,3 +126,128 @@ function cancel(val){
     child.remove(parent);
 }
 
+
+
+
+
+
+
+
+
+
+
+
+let indicatorsuggestions = [["MA","Moving Average"],["EMA","Exponential Moving Average"],["DEMA","Double Moving Average"],["MACD","Moving Average Crossover Divergion"]];
+
+var indicatorWrapper = document.querySelector(".indicator-search");
+var indicatorInputBox = indicatorWrapper.querySelector("input");
+var indicatorSuggBox;
+
+function lookup(arg){
+    var id = arg.getAttribute('id');
+    var no = id.charAt(id.length-1);
+    var value = arg.value;
+    console.log(no);
+    let userData = value;
+    let emptyArray = [];
+    let dataArray = [];
+    var string = "#indicatorBox"+no.toString();
+    console.log(string);
+    indicatorSuggBox = indicatorWrapper.querySelector(string);
+    // console.log(indicatorSuggBox);
+    if(userData){
+        emptyArray = indicatorsuggestions.filter((data)=>{
+            return data[0].toLocaleLowerCase().startsWith(userData.toLocaleLowerCase());
+        });
+        dataArray = emptyArray;
+        emptyArray = emptyArray.map((data)=>{
+            return data = `<li></li>`;
+        });
+        indicatorWrapper.classList.add("active");
+        showIndicatorSuggestions(emptyArray);
+        let allList = indicatorSuggBox.querySelectorAll("li");
+        for (let i = 0; i < allList.length; i++) {
+            let head = document.createElement("p");
+            let desc = document.createElement("p");
+            head.innerHTML = dataArray[i][0];
+            desc.innerHTML = dataArray[i][1]
+            head.classList.add("indicatorsuggestionHead");
+            desc.classList.add("indicatorsuggestionDesc");
+            allList[i].appendChild(head);
+            allList[i].appendChild(desc);
+            allList[i].setAttribute("name", 'this.innerText');
+            allList[i].setAttribute("onclick", "openIndicatorDetails('"+dataArray[i][0]+"',"+no+")");
+        }
+    }else{
+        indicatorWrapper.classList.remove("active");
+        dataArray = [];
+    }
+}
+
+
+function showIndicatorSuggestions(list){
+    let listData;
+    if(!list.length){
+        userValue = indicatorInputBox.value;
+        listData = `<li>${userValue}</li>`;
+    }else{
+      listData = list.join('');
+    }
+    indicatorSuggBox.innerHTML = listData;
+}
+
+function openIndicatorDetails(element,id){
+    $('#indicatorModal').modal();
+    let x = document.getElementById('indicatorheadingModal');
+    x.innerHTML = element;
+    console.log("Id "+id);
+    let y = document.getElementById('modalIndicator');
+    y.setAttribute("onclick","saveIndicatordetails("+id+");")
+}
+
+
+function saveIndicatordetails(id){
+
+    var x = document.getElementsByName("period1")[0].value;
+    var y = document.getElementsByName("period2")[0].value;
+    var z = document.getElementById('indicatorheadingModal').innerHTML;
+    console.log(id);
+    document.getElementsByName("entryfirindicator"+id)[0].value = "("+z+","+x+")";
+    document.getElementsByName("entrysecindicator"+id)[0].value = "("+z+","+y+")";
+    indicatorWrapper.classList.remove("active");
+    $('#indicatorModal').modal('hide');
+}
+
+
+console.log("Here");
+
+
+
+
+
+function addRow() {
+  const div = document.createElement('div');
+
+  div.className = 'row';
+
+  div.innerHTML =  `<div class="row" id="inputExitFormRow"><div class="col-lg-4 col-xl-4 col-md-12 col-sm-12 col-xs-12">`+
+                               `<input type="text" name="exitindiactorone" placeholder="Indicator" class="form-control">`+
+                               `<ul class="indicatorsuggest-box" id="indicatorBox"></ul>`+
+                               `</div><div class="col-lg-4 col-xl-3 col-md-12 col-sm-12 col-xs-12">`+
+                               `<input type="text" name="exitcomparator" placeholder="Comparator" class="form-control">`+
+                               `</div><div class="col-lg-4 col-xl-4 col-md-12 col-sm-12 col-xs-12">`+
+                               `<input type="text" name="exitindiactortwo" placeholder="Indicator" class="form-control"></div>`+
+                                `<div class="col-1">`+
+                               `<span class="w-50 h-50 deleteExitRow" style="font-size:3rem" onclick="removeRow(this)">&times;</span>`+
+                               `</div>`+
+                               `</div>`
+
+
+  document.getElementById('entryConditionRow').appendChild(div);
+}
+
+
+function removeRow(input) {
+    console.log(input)
+    input.parentNode.parentNode.remove()
+}
